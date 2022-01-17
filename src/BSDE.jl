cabfa1c4f8976d32d55be51227192086a898c60f
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-01-17 14:06:51 +0100

    update to BSDE

--- src/BSDE.jl
+++ src/BSDE.jl
@@ -3,10 +3,13 @@
 # In the future also more general lattices will be dealt with
 
 
-function dynamicRM(process::SDEProblem,eval = x-> sum(x), RM=0.0, u0::Flux.Chain=Flux.Chain(), σᵀ∇u::Flux.Chain=Flux.Chain())
+function dynamicRM(prob2::SDEProblem,eval = x-> sum(x), RM=0.0, u0::Flux.Chain=Flux.Chain(), σᵀ∇u::Flux.Chain=Flux.Chain())
     # get information on forward process from SDEProblem
 
     # get information on RM from class RM which riskMeasures introduces
+
+    dt = 0.2   # time step
+    time_steps = div(prob2.tspan[2]-prob2.tspan[1],dt, RoundNearest)
 
 
     f(X,u,σᵀ∇u,p,t) = abs.(σᵀ∇u)[1]                 # nonlinear riskMeasure part
@@ -23,15 +26,14 @@ function dynamicRM(process::SDEProblem,eval = x-> sum(x), RM=0.0, u0::Flux.Chain
                 Dense(hls,1))
     end
     if (σᵀ∇u == Flux.Chain())
-        σᵀ∇u = Flux.Chain(Dense(d+1,hls,relu),
-                  Dense(hls,hls,relu),
-                  Dense(hls,hls,relu),
-                  Dense(hls,hls,relu),
-                  Dense(hls,d))
+        σᵀ∇u = [Flux.Chain(Dense(d,hls,relu),
+            Dense(hls,hls,relu),
+            Dense(hls,d)) for i in 1:time_steps]
     end
 
 
-    pdealg = NNPDENS(u0, σᵀ∇u, opt=opt)
-    ans = solve(prob, pdealg, verbose=true, maxiters=50, trajectories=100,alg=EM(), dt=0.2, pabstol = 1f-6)
+
+    pdealg = NNPDEHan(u0,σᵀ∇u;opt=Flux.ADAM(0.1))
+    ans = solve(prob, pdealg, verbose=true, maxiters=50, trajectories=100, dt=dt)
     return(ans)
 end
---
+++ src/test.jl
@@ -0,0 +1,35 @@
+using Flux, LinearAlgebra
+println("NNPDEHAN_tests")
+using NeuralPDE
+
+using Random
+Random.seed!(100)
+
+# one-dimensional heat equation
+x0 = [11.0f0]  # initial points
+tspan = (0.0f0,5.0f0)
+dt = 0.01   # time step
+time_steps = div(tspan[2]-tspan[1],dt, RoundNearest)
+d = 1      # number of dimensions
+m = 10     # number of trajectories (batch size)
+
+g(X) = sum(X.^2)   # terminal condition
+f(X,u,σᵀ∇u,p,t) = 0.0  # function from solved equation
+μ_f(X,p,t) = 0.0
+σ_f(X,p,t) = 1.0
+prob = TerminalPDEProblem(g, f, μ_f, σ_f, x0, tspan)
+
+hls = 10 + d #hidden layer size
+opt = Flux.ADAM(0.005)  #optimizer
+#sub-neural network approximating solutions at the desired point
+u0 = Flux.Chain(Dense(d,hls,relu),
+                Dense(hls,hls,relu),
+                Dense(hls,1))
+# sub-neural network approximating the spatial gradients at time point
+σᵀ∇u = [Flux.Chain(Dense(d,hls,relu),
+                  Dense(hls,hls,relu),
+                  Dense(hls,d)) for i in 1:time_steps]
+
+alg = NNPDEHan(u0, σᵀ∇u, opt = opt)
+
+ans = solve(prob, alg, verbose = true, abstol=1e-8, maxiters = 200, dt=dt, trajectories=m)
