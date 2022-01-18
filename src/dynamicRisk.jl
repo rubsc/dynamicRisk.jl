0122a2da66743a4d6c1a6f0c648ce20adfafb43a
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-01-19 00:22:09 +0100

    changed BSDE and tests

--- src/dynamicRisk.jl
+++ src/dynamicRisk.jl
@@ -2,8 +2,8 @@ module dynamicRisk
 
 
 using Reexport
-#@reexport using DifferentialEquations
-#@reexport using ScenTrees
+@reexport using DifferentialEquations
+@reexport using ScenTrees
 
 using LinearAlgebra, NeuralPDE, Flux
 using Test
--- src/tree.jl
+++ src/tree.jl
@@ -4,25 +4,25 @@
 Implements the nested version of RM for the tree specified by tree0.
 
 """
-#function dynamicRM(tree0::tree,eval = x-> sum(x), RM=1.0 )
+function dynamicRM(tree0::tree,eval = x-> sum(x), RM=1.0 )
 
 # recursively go through tree and apply RM
-#Tree1 = deepcopy(Tree0);
+Tree1 = deepcopy(Tree0);
 
 # For every parent node going backwards
-#tmp = reverse(unique(Tree2.parent)); pop!(tmp)
-#for i in tmp
+tmp = reverse(unique(Tree2.parent)); pop!(tmp)
+for i in tmp
     #Note that the probs are the conditional ones
     #Risk measure w/ adjustment of values
-#    states2 = Tree2.state[Tree2.children[i+1]]
-#    prob2 = Tree2.probability[Tree2.children[i+1]]
+    states2 = Tree2.state[Tree2.children[i+1]]
+    prob2 = Tree2.probability[Tree2.children[i+1]]
 
-#    Tree2.state[i] = mSD(0.95,5,states2,prob2)
-#end
+    Tree2.state[i] = mSD(0.95,5,states2,prob2)
+end
 
 
-#return(nothing)
-#end
+return(nothing)
+end
 
 
 
