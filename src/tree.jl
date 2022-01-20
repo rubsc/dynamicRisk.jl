3f9e0c16cf6d1bfa937435dc0dac749f11f26d60
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-01-20 23:30:19 +0100

    added risk measures for trees

--- src/tree.jl
+++ src/tree.jl
@@ -1,32 +1,32 @@
 """
-    dynamicRM(tree0::tree,eval = x-> sum(x), RM=1.0 ) 
+    dynamicRM(trr::tree, RM::Array{Any,1} ) 
 
-Implements the nested version of RM for the tree specified by tree0.
+Implements the nested version of RM for the tree specified by trr.
+The user has to specify a vector of risk measures of lenght N, where N is the number of stages of trr. 
+Furthermore the risk measures RM[i] should be of the form RM[i](states, prob). 
 
 """
-function dynamicRM(trr,eval = x-> sum(x); RM=1.0 )
+function dynamicRM(trr::Tree; RM::Array{Any,1})
 
-    println(typeof(RM))
-#    if typeof(RM) == func
-
+    if !(length(RM) == maximum(stage(trr)) )
+        println("Error, number of risk measures should be equal to the number of stages")
+        return(nothing)
+    end
 # recursively go through tree and apply RM
-#T0 = deepcopy(trr);
+    T0 = deepcopy(trr);
 
-# For every parent node going backwards
-#tmp = reverse(unique(T0.parent)); pop!(tmp)
-#for i in tmp
-    #Note that the probs are the conditional ones
-    #Risk measure w/ adjustment of values
-#    states2 = T0.state[T0.children[i+1]]
-#    prob2 = T0.probability[T0.children[i+1]]
-
-
-
-#    T0.state[i] = mSD(states2,prob2,0.95,5)
-#end
+    # For every parent node going backwards
+    tmp = reverse(unique(T0.parent)); pop!(tmp)
+    for i in tmp
+        #Note that the probs are the conditional ones
+        #Risk measure w/ adjustment of values
+        states = T0.state[T0.children[i+1]]
+        prob = T0.probability[T0.children[i+1]]
+        T0.state[i] = RM[i](states,prob)
+    end
 
 
-return(nothing)
+    return(T0[1],T0)
 end
 
 
@@ -38,3 +38,132 @@ end
     
 #    return(nothing)
 #end
+
+
+
+""" 
+    VaR(states,prob,alpha)
+implements the Value-at-Risk at level ``\\alpha`` defined by
+```math
+VaR_\\alpha (Y) = \\arg \\min_x \\left( x\\in \\mathbb{R} : F_Y(x) \\geq \\alpha \\right),
+```
+for the random variable ``Y`` defined by `states` and `prob`.
+"""
+function VaR(trr::Tree,alpha::Float64)
+    # look for smallest x such that  P(-states <= x) >alpha , i.e.
+    T0 = deepcopy(trr);
+
+    # For every parent node going backwards
+    tmp = reverse(unique(T0.parent)); pop!(tmp)
+    for i in tmp
+        #Note that the probs are the conditional ones
+        #Risk measure w/ adjustment of values
+        states = T0.state[T0.children[i+1]]
+        prob = T0.probability[T0.children[i+1]]
+
+        T0.state[i] = VaR(states,prob,alpha)
+    end
+    return(T0.state[1],T0)
+end
+
+""" 
+    CTE2(states,prob,alpha)
+implements the Conditional Value-at-Risk at level ``\\alpha`` defined by
+```math
+CTE_\\alpha (Y) = VaR_\\alpha(Y) + \\frac{1}{1-\\alpha} \\mathbb{E} \\left( Y- VaR_\\alpha (Y) \\right)_+ ,
+```
+for the random variable ``Y`` defined by `states` and `prob`.
+"""
+function CTE(trr::Tree,alpha::Float64)
+    T0 = deepcopy(trr);
+
+    # For every parent node going backwards
+    tmp = reverse(unique(T0.parent)); pop!(tmp)
+    for i in tmp
+        #Note that the probs are the conditional ones
+        #Risk measure w/ adjustment of values
+        states = T0.state[T0.children[i+1]]
+        prob = T0.probability[T0.children[i+1]]
+
+        T0.state[i] = CTE(states,prob,alpha)
+    end
+    return(T0.state[1],T0)
+end
+
+
+
+"""
+    EVaR2(states,prob,beta)
+Solves the optimization problem associated with the primal formulation of the Entropic Value-at-Risk:
+```math
+EVaR_\\alpha(Y) = \\min_{x >0} \\frac{1}{x} \\left( \\beta +  \\log\\mathbb{E} e^{xY} \\right),
+```
+where ``Y`` is the discrete random variable defined by `states` and `prob`.
+Here the optimization is done via the goldenSearch optimization routine implemented as part of this package. 
+"""
+function EVaR2(trr::Tree,beta::Float64)
+	T0 = deepcopy(trr);
+
+    # For every parent node going backwards
+    tmp = reverse(unique(T0.parent)); pop!(tmp)
+    for i in tmp
+        #Note that the probs are the conditional ones
+        #Risk measure w/ adjustment of values
+        states = T0.state[T0.children[i+1]]
+        prob = T0.probability[T0.children[i+1]]
+
+        T0.state[i] = EVaR2(states,prob,beta)
+    end
+    return(T0.state[1],T0)
+end
+
+
+"""
+    EVaR(states,prob,beta)
+Solves the optimization problem associated with the primal formulation of the Entropic Value-at-Risk:
+```math
+EVaR_\\alpha(Y) = \\min_{x >0} \\frac{1}{x} \\left( \\beta +  \\log\\mathbb{E} e^{xY} \\right),
+```
+where ``Y`` is the discrete random variable defined by `states` and `prob`. Here, the optimization is done using JuMP and Ipopt.  
+"""
+function EVaR(trr::Tree,beta::Float64)
+	T0 = deepcopy(trr);
+
+    # For every parent node going backwards
+    tmp = reverse(unique(T0.parent)); pop!(tmp)
+    for i in tmp
+        #Note that the probs are the conditional ones
+        #Risk measure w/ adjustment of values
+        states = T0.state[T0.children[i+1]]
+        prob = T0.probability[T0.children[i+1]]
+
+        T0.state[i] = EVaR(states,prob,beta)
+    end
+    return(T0.state[1],T0)
+end
+
+
+
+"""
+    AVaR(states,prob,alpha)
+Solves the optimization problem associated with the primal formulation of the Average Value-at-Risk:
+```math
+AVaR_\\alpha(Y) = \\min_{x\\in \\mathbb{R}} x + \\frac{1}{1-\\alpha} \\mathbb{E} \\left( Y - x \\right)_+,
+```
+where ``Y`` is the discrete random variable defined by `states` and `prob`.
+"""
+function AVaR(trr::Tree, alpha::Float64)
+    T0 = deepcopy(trr);
+
+    # For every parent node going backwards
+    tmp = reverse(unique(T0.parent)); pop!(tmp)
+    for i in tmp
+        #Note that the probs are the conditional ones
+        #Risk measure w/ adjustment of values
+        states = T0.state[T0.children[i+1]]
+        prob = T0.probability[T0.children[i+1]]
+
+        T0.state[i] = AVaR(states,prob,alpha)
+    end
+    return(T0.state[1],T0)
+end
