77e4b26f7ac8b95d0826346cd8ecfffc6370e6c5
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-01-24 21:36:10 +0100

    updated import/export

--- src/dynamicRisk.jl
+++ src/dynamicRisk.jl
@@ -1,10 +1,11 @@
 module dynamicRisk
 
+using Pkg; Pkg.add("https://github.com/rubsc/riskMeasures.jl")
 using Reexport
 @reexport using DifferentialEquations
 @reexport using ScenTreesMakie
 
-import riskMeasures: VaR, AVaR, EVaR, EVaR2, CTE
+import riskMeasures
 
 using LinearAlgebra, NeuralPDE, Flux
 using Test
@@ -15,7 +16,8 @@ include("tree.jl")
 include("lattice.jl")
 
 
-export dynamicRM, Var, CTE, AVaR, EVaR, EVaR2
+export dynamicRM, Var, CTE, AVaR, EVaR, EVaR2, Expectation, entropic,
+        mSD, meanVariance, meanDeviation, meanSemiVariance, meanSemiDevi
 
 
 end # module
